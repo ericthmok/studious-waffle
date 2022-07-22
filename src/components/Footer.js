8f2785cb3c09ed4fe6bf345b5d24ba2b81cc9c8e
Eric Mok <ericmok@Erics-MacBook-Air.local>
2022-07-22 14:13:12 -0400

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,13 @@ function Footer(){
             <a href='https://github.com/ericthmok'>GitHub</a><br></br>
             <a href='https://www.linkedin.com/in/ericmokth/'>Linkedin</a><br></br>
             <a href='#'>647-212-0920</a><br></br>
-            <a href='#'>ericthmok@gmail.com</a><br></br><br></br><br></br>
+            <a href='#'>ericthmok@gmail.com</a><br></br><br></br>
+            <form>
+                <a>Leave me a message:</a><br></br>
+                <input placeholder="Name"></input><br></br>
+                <input placeholder="email"></input><br></br>
+                <input placeholder="Message to me"></input>
+            </form><br></br><br></br>
             <a href='#head'>HOME</a>
         </footer>
     )
